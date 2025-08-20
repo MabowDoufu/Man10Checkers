5d0b06d94cbd313159af410d011ab461af221b00
MabowDoufu <7mabou.douhu@gmail.com>
2025-08-20 10:20:15 +0900

    BoardGameSys連続駒とびまで作成

--- src/main/mabowdoufu/BoardGameSys.java
+++ src/main/mabowdoufu/BoardGameSys.java
@@ -3,7 +3,12 @@ import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.spi.AbstractResourceBundleProvider;
+import static java.lang.Math.*;
 
 public class BoardGameSys {
     // 盤面情報
@@ -63,7 +68,7 @@ public class BoardGameSys {
         }
     }
 
-    public static boolean IsMyMen(int[][] board,int playermen,int x1, int y1, int x2, int y2 ){
+    public static boolean IsMyMen(int[][] board,int playermen,int x1, int y1){
 
         int selectmen = board[x1][y1];
         //相手の駒選択しているかどうか
@@ -75,8 +80,81 @@ public class BoardGameSys {
         }
         return true;
     }
-    //相手の駒を破壊可能な手があり、その場合にその手を選択しているかどうか
-    public static boolean IsBreakable(int[][] board,int playermen,int x1,int y1,int x2,int y2){
+
+    //directionは1または-1のみをとる
+    public static boolean IsJumpable(int[][] board,int playermen,int x1,int y1,int xdirection,int ydirection){
+        int enemymen;
+        if(playermen==1){
+            enemymen= 2;
+        }else{
+            enemymen=1;
+        }
+        try {
+            if (board[x1 + xdirection][y1 +ydirection] == enemymen && board[x1 + 2*xdirection][y1 + 2*ydirection] == 0) {
+                return true;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+    public static void ContinuousMove(int[][] board,int playermen, int x1,int y1,boolean IsKing){
+        List<Integer> Movable = new ArrayList<Integer>();
+        if(playermen==1 ||(playermen==2 && IsKing)) {
+            try {
+                if(ExistJumpMove(board, playermen, x1, y1, x1 + 2, y1 + 2)){
+                    Movable.add(1);
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                e.printStackTrace();
+            }
+            try {
+                if(ExistJumpMove(board, playermen, x1, y1, x1 - 2, y1 + 2)){
+                    Movable.add(2);
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                e.printStackTrace();
+            }
+        }
+        if(playermen==2 ||(playermen==1 && IsKing)) {
+            try {
+                if(ExistJumpMove(board, playermen, x1, y1, x1 + 2, y1 - 2)){
+                    Movable.add(3);
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                e.printStackTrace();
+            }
+            try {
+                if(ExistJumpMove(board, playermen, x1, y1, x1 - 2, y1 - 2)){
+                    Movable.add(4);
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                e.printStackTrace();
+            }
+        }
+        Random random = new Random();
+        int movePattern = Movable.get(random.nextInt(Movable.size()-1)); // 0からMovable.size()-1までの整数を生成
+        if(movePattern ==1){
+            board[x1][y1]= 0;
+            board[x1+2][y1+2]= playermen;
+            ContinuousMove(board,playermen,x1+2,y1+2,IsKing);
+        }else if(movePattern ==2){
+            board[x1][y1]= 0;
+            board[x1-2][y1+2]= playermen;
+            ContinuousMove(board,playermen,x1-2,y1+2,IsKing);
+        }else if(movePattern ==3) {
+            board[x1][y1] = 0;
+            board[x1 + 2][y1 - 2] = playermen;
+            ContinuousMove(board,playermen,x1+2,y1-2,IsKing);
+        }else if(movePattern ==4) {
+            board[x1][y1] = 0;
+            board[x1 - 2][y1 - 2] = playermen;
+            ContinuousMove(board,playermen,x1-2,y1-2,IsKing);
+        }
+    }
+    //相手の駒をジャンプ可能な手があり、その場合にその手を選択しているかどうか
+    public static boolean ExistJumpMove(int[][] board,int playermen,int x1,int y1,int x2,int y2){
         //yml読み込み
         int enemymen;
         if(playermen==1){
@@ -87,56 +165,32 @@ public class BoardGameSys {
         //placeableチェック
         int checkX=0;
         int checkY=0;
-        boolean isBreakable=false;
+        boolean exist=false;
         for(int[] row : board ) {
             for(int men : row) {
                 if(men == playermen) {
-                    if( x2-x1==2 &&y2-y1 == 2) {
-                        try {
-                            if (board[checkX + 1][checkY + 1] == enemymen && board[checkX + 2][checkY + 2] == 0) {
-                                isBreakable = true;
-                                if ((checkX == x1) && (checkY == y1)) {
-                                    return true;
-                                }
-                            }
-                        } catch (ArrayIndexOutOfBoundsException e) {
-                            e.printStackTrace();
+                    if(IsJumpable(board,playermen,checkX,checkY,1,1)){
+                        exist = true;
+                        if ((checkX == x1) && (checkY == y1)) {
+                            return true;
                         }
                     }
-                    if( x2-x1==-2 &&y2-y1 == 2) {
-                        try {
-                            if (board[checkX - 1][checkY + 1] == enemymen && board[checkX - 2][checkY + 2] == 0) {
-                                isBreakable = true;
-                                if ((checkX == x1) && (checkY == y1)) {
-                                    return true;
-                                }
-                            }
-                        } catch (ArrayIndexOutOfBoundsException e) {
-                            e.printStackTrace();
+                    if(IsJumpable(board,playermen,checkX,checkY,-1,1)){
+                        exist = true;
+                        if ((checkX == x1) && (checkY == y1)) {
+                            return true;
                         }
                     }
-                    if( x2-x1==2 &&y2-y1 == -2) {
-                        try {
-                            if (board[checkX + 1][checkY - 1] == enemymen && board[checkX + 2][checkY - 2] == 0) {
-                                isBreakable = true;
-                                if ((checkX == x1) && (checkY == y1)) {
-                                    return true;
-                                }
-                            }
-                        } catch (ArrayIndexOutOfBoundsException e) {
-                            e.printStackTrace();
+                    if(IsJumpable(board,playermen,checkX,checkY,1,-1)){
+                        exist = true;
+                        if ((checkX == x1) && (checkY == y1)) {
+                            return true;
                         }
                     }
-                    if( x2-x1==-2 &&y2-y1 == -2) {
-                        try {
-                            if (board[checkX - 1][checkY - 1] == enemymen && board[checkX - 2][checkY - 2] == 0) {
-                                isBreakable = true;
-                                if ((checkX == x1) && (checkY == y1)) {
-                                    return true;
-                                }
-                            }
-                        } catch (ArrayIndexOutOfBoundsException e) {
-                            e.printStackTrace();
+                    if(IsJumpable(board,playermen,checkX,checkY,-1,-1)){
+                        exist = true;
+                        if ((checkX == x1) && (checkY == y1)) {
+                            return true;
                         }
                     }
                 }
@@ -144,7 +198,7 @@ public class BoardGameSys {
             }
             checkX++;
         }
-        return !isBreakable;
+        return !exist;
     }
     //駒をおけるか
     public static void BoardInput(String boardname,int playermen,int x1, int y1, int x2, int y2 ){
@@ -159,11 +213,24 @@ public class BoardGameSys {
         int selectmen = board[x1][y1];
 
         //破壊可能な手があるのにも関わらずその手を選択していない場合を除外
-        if(!IsBreakable(board,playermen,x1,y1,x2,y2)){
+        if(!ExistJumpMove(board,playermen,x1,y1,x2,y2)){
+            //相手の駒を飛び越えられる手が存在します。飛び越えられる手を選択してください。
             return;
         }
 
         //
+        if(abs(x1-x2)==1 && abs(y1-y2)==1){
+
+        } else if(abs(x1-x2)==2 && abs(y1-y2)==2){
+            if(IsJumpable(board,playermen,x1,y1,x2-x1,y2-y1)){
+                board[x1][y1]= 0;
+                board[x2][y2]= playermen;
+
+            }
+        }else{
+            //最初に選択した駒の一つ斜めの駒か、相手の駒を飛び越えられる場合は二つ斜め前の駒を選択してください。
+            return;
+        }
 
 
         saveBoard(boardname,board);
